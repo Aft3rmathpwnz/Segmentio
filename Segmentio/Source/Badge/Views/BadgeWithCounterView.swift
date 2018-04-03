aae3a736514f3ce9bdace2a82d7d331273b1c015
Aft3rmath <aft3rmathpwnz@gmail.com>
2018-04-03 11:23:30 +0300

    Badge with gradient colors

--- Segmentio/Source/Badge/Views/BadgeWithCounterView.swift
+++ Segmentio/Source/Badge/Views/BadgeWithCounterView.swift
@@ -16,6 +16,8 @@ class BadgeWithCounterView: UIView {
     
     @IBOutlet fileprivate weak var counterValueLabel: UILabel!
     @IBOutlet fileprivate weak var backgroundImageView: UIImageView!
+    private var areSubviewsLayouted = false
+    private var gradientColors: [UIColor]?
     
     class func instanceFromNib(size: BadgeSize) -> BadgeWithCounterView {
         let nibName = nibNameForSize(size)
@@ -27,6 +29,33 @@ class BadgeWithCounterView: UIView {
                 .instantiate(withOwner: nil, options: nil)[0] as! BadgeWithCounterView
         }
         return BadgeWithCounterView(frame: .zero)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if areSubviewsLayouted == false {
+            print("layoutSubviews")
+            print("backgroundImageView frame - \(backgroundImageView.frame)")
+            if gradientColors != nil {
+                let gradient = CAGradientLayer()
+                gradient.frame = backgroundImageView.bounds
+                gradient.colors = gradientColors!.map({ $0.cgColor })
+                gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+                gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+                
+                let testLayer = CALayer()
+                testLayer.frame = backgroundImageView.bounds
+                testLayer.backgroundColor = gradientColors![1].cgColor
+                
+                let maskLayer = CAShapeLayer()
+                maskLayer.path = UIBezierPath(roundedRect: backgroundImageView.bounds, byRoundingCorners: [.topLeft, .topRight, .bottomLeft, .bottomRight], cornerRadii: CGSize(width: backgroundImageView.bounds.height / 2.0, height: backgroundImageView.bounds.height / 2.0)).cgPath
+                gradient.mask = maskLayer
+                
+                backgroundImageView.layer.insertSublayer(gradient, at: 0)
+                areSubviewsLayouted = true
+            }
+        }
+        
     }
 
     func setBadgeCounterValue(_ counterValue: Int) {
@@ -47,12 +76,9 @@ class BadgeWithCounterView: UIView {
     }
     
     func setBadgeBackgroundColors(_ gradientColors: [UIColor]) {
-        let gradient = CAGradientLayer()
-        gradient.frame = backgroundImageView.bounds
-        gradient.colors = gradientColors
-        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
-        gradient.endPoint = CGPoint(x: 1.0, y: 0.0)
-        backgroundImageView.layer.insertSublayer(gradient, at: 0)
+        self.gradientColors = gradientColors
+        
+        //backgroundImageView.backgroundColor = gradientColors[0]
     }
     
     fileprivate class func nibNameForSize(_ size: BadgeSize) -> String {
